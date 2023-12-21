390a9ac03f2c31b62f101b2dc3117930305101a4
pihaf <113176707+pihaf@users.noreply.github.com>
2023-12-21 16:13:51 +0700

    Update controller

---
+++ src/controllers/AdminController.js
@@ -0,0 +1,64 @@
+const Admin = require('../models/Admin');
+
+// Create a new admin
+exports.createAdmin = async (req, res) => {
+    // Allow access only to admins
+  if (req.model !== 'admin') {
+      return res.status(403).json({ error: 'Forbidden' });
+  }
+  const { name, email, username, password } = req.body;
+
+  try {
+    // Check if the username already exists
+    const existingAdmin = await Admin.findOne({ where: { username } });
+    if (existingAdmin) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create the admin
+    const admin = await Admin.create({
+      name,
+      email,
+      username,
+      password: hashedPassword
+    });
+
+    // Generate JWT token for the admin
+    const newToken = admin.generateAuthToken();
+
+    res.status(201).json({ admin, token: newToken });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create admin' });
+  }
+};
+
+// Get admin profile
+exports.getAdminProfile = async (req, res) => {
+  try {
+    const admin = await Admin.verifyAuthToken(req.token);
+    res.json(admin);
+  } catch (error) {
+    res.status(401).json({ error: 'Unauthorized' });
+  }
+};
+
+// Update admin profile
+exports.updateAdminProfile = async (req, res) => {
+  try {
+    const admin = await Admin.verifyAuthToken(req.token);
+
+    // Update the admin information
+    admin.name = req.body.name;
+    admin.email = req.body.email;
+    admin.username = req.body.username;
+
+    await admin.save();
+
+    res.json(admin);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+};
--- src/routers/adminRouter.js
+++ src/routers/adminRouter.js
@@ -5,40 +5,7 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = require('../middleware/auth');
 const Admin = require('../models/Admin');
 
-// Create a new admin
-router.post('/api/admin/register', authenticateToken, async (req, res) => {
-    // Allow access only to admins
-  if (req.model !== 'admin') {
-      return res.status(403).json({ error: 'Forbidden' });
-  }
-  const { name, email, username, password } = req.body;
-
-  try {
-    // Check if the username already exists
-    const existingAdmin = await Admin.findOne({ where: { username } });
-    if (existingAdmin) {
-      return res.status(400).json({ error: 'Username already exists' });
-    }
-
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Create the admin
-    const admin = await Admin.create({
-      name,
-      email,
-      username,
-      password: hashedPassword
-    });
-
-    // Generate JWT token for the admin
-    const newToken = admin.generateAuthToken();
-
-    res.status(201).json({ admin, token: newToken });
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to create admin' });
-  }
-});
+const adminController = require('../controllers/adminController');
 
 // Admin login
 router.post('/api/admin/login', async (req, res) => {
@@ -77,33 +44,13 @@ router.post('/api/admin/logout', authenticateToken, (req, res) => {
   }
 });
 
+// Create a new admin
+router.post('/api/admin/register', authenticateToken, adminController.createAdmin);
 
 // Get admin profile
-router.get('/api/admin/profile', authenticateToken, async (req, res) => {
-  try {
-    const admin = await Admin.verifyAuthToken(req.token);
-    res.json(admin);
-  } catch (error) {
-    res.status(401).json({ error: 'Unauthorized' });
-  }
-});
+router.get('/api/admin/profile', authenticateToken, adminController.getAdminProfile);
 
 // Update admin profile
-router.put('/api/admin/profile', authenticateToken, async (req, res) => {
-  try {
-    const admin = await Admin.verifyAuthToken(req.token);
-
-    // Update the admin information
-    admin.name = req.body.name;
-    admin.email = req.body.email;
-    admin.username = req.body.username;
-
-    await admin.save();
-
-    res.json(admin);
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' });
-  }
-});
+router.put('/api/admin/profile', authenticateToken, adminController.updateAdminProfile);
 
 module.exports = router;
