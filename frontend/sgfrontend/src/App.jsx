07bb528072542d8821becd8795a34894e664ddd3
pihaf <113176707+pihaf@users.noreply.github.com>
2023-12-24 22:39:35 +0700

    Add routes and test nav bar

--- frontend/sgfrontend/src/App.jsx
+++ frontend/sgfrontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './components/Home';
+import Navbar from './components/Navbar.jsx';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Curriculum from './components/Curriculum';
@@ -15,6 +16,7 @@ function App() {
   return (
     <Router>
       <Header />
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
