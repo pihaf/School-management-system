34b89f96d6e4cb4330bcb673733d12b988d6fb57
Nguyen Le Linh Chi <chinguyencarat@gmail.com>
2023-12-28 01:18:49 +0700

    Modify user Header and Footer

--- frontend/sgfrontend/src/App.jsx
+++ frontend/sgfrontend/src/App.jsx
@@ -1,35 +1,42 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import Home from './components/Home';
-import Navbar from './components/Navbar.jsx';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Timetable from './components/Timetable';
-import Course from './components/Course';
-import Request from './components/Request';
-import Notification from './components/Notification';
-import Profile from './components/Profile';
-import Login from './components/Login';
-import Logout from './components/Logout'; 
-import NoPage from './components/NoPage.jsx';
-import AdminLogin from './components/admin/AdminLogin.jsx';
-import AdminLogout from './components/admin/AdminLogout.jsx';
-import AdminHome from './components/admin/AdminHome.jsx';
-import Dashboard from './components/admin/Dashboard.jsx'
-import AdminProfile from './components/admin/AdminProfile.jsx';
-import Students from './components/admin/Students.jsx';
-import Lecturers from './components/admin/Lecturers.jsx';
-import News from './components/admin/News.jsx';
-import AdminRequest from './components/admin/AdminRequest.jsx';
-import AdminCourse from './components/admin/AdminCourse.jsx';
-import Grade from './components/admin/Grade.jsx';
+import React from "react";
+import { useEffect, useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar.jsx";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Timetable from "./components/Timetable";
+import Course from "./components/Course";
+import Request from "./components/Request";
+import Notification from "./components/Notification";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
+import NoPage from "./components/NoPage.jsx";
+import AdminLogin from "./components/admin/AdminLogin.jsx";
+import AdminLogout from "./components/admin/AdminLogout.jsx";
+import AdminHome from "./components/admin/AdminHome.jsx";
+import Dashboard from "./components/admin/Dashboard.jsx";
+import AdminProfile from "./components/admin/AdminProfile.jsx";
+import Students from "./components/admin/Students.jsx";
+import Lecturers from "./components/admin/Lecturers.jsx";
+import News from "./components/admin/News.jsx";
+import AdminRequest from "./components/admin/AdminRequest.jsx";
+import AdminCourse from "./components/admin/AdminCourse.jsx";
+import Grade from "./components/admin/Grade.jsx";
 
 function App() {
-  const [token, setToken] = useState(localStorage.getItem('token'));
-  const [adminToken, setAdminToken] = useState(localStorage.getItem('adminToken'));
-  const [model, setModel] = useState(localStorage.getItem('model'));
-  const [id, setId] = useState(localStorage.getItem('id'));
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const [adminToken, setAdminToken] = useState(
+    localStorage.getItem("adminToken")
+  );
+  const [model, setModel] = useState(localStorage.getItem("model"));
+  const [id, setId] = useState(localStorage.getItem("id"));
 
   // useEffect(() => {
   //   const token = localStorage.getItem('token');
@@ -47,70 +54,190 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<>
-              <Header />
+        <Route
+          exact
+          path="/"
+          element={
+            <>
+              <Header model={model} id={id} />
               <Navbar />
               <Home />
               <Footer />
-            </>} />
-        <Route path="/login" element={<>
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Login setToken={setToken} setModel={setModel} setId={setId}/>
+              <Login setToken={setToken} setModel={setModel} setId={setId} />
               <Footer />
-            </>} />
-        <Route path="/logout" element={<>
+            </>
+          }
+        />
+        <Route
+          path="/logout"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Logout setToken={setToken} setModel={setModel} setId={setId} /> 
+              <Logout setToken={setToken} setModel={setModel} setId={setId} />
               <Footer />
-        </>} />
-        <Route path="/timetable" element={<>
+            </>
+          }
+        />
+        <Route
+          path="/timetable"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Timetable isAuthenticated={!!token} model={model} id={id}/>
-              <Footer/>
-        </>} />
-        <Route path="/course" element={<>
+              <Timetable isAuthenticated={!!token} model={model} id={id} />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/course"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Course isAuthenticated={!!token} model={model} id={id}/>
-              <Footer/>
-        </>} />
-        <Route path="/request" element={<>
+              <Course isAuthenticated={!!token} model={model} id={id} />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/request"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Request isAuthenticated={!!token} model={model} id={id}/>
-              <Footer/>
-        </>} />
-        <Route path="/notification" element={<>
+              <Request isAuthenticated={!!token} model={model} id={id} />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/notification"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Notification isAuthenticated={!!token} model={model} id={id}/>
-              <Footer/>
-        </>} />
-        <Route path="/profile" element={<>
+              <Notification isAuthenticated={!!token} model={model} id={id} />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <>
               <Header />
               <Navbar />
-              <Profile isAuthenticated={!!token} model={model} id={id}/>
-              <Footer/>
-        </>} />
+              <Profile isAuthenticated={!!token} model={model} id={id} />
+              <Footer />
+            </>
+          }
+        />
         <Route path="*" element={<NoPage />} />
         {/* For admins */}
-        <Route path="/admin" element={<AdminHome isAuthenticated={!!adminToken} adminToken={adminToken} setAdminToken={setAdminToken}/>} />
-        <Route path="/admin/login" element={<AdminLogin setAdminToken={setAdminToken}/>} />
-        <Route path="/admin/logout" element={<AdminLogout setAdminToken={setAdminToken}/>} />
-        <Route path="/admin/dashboard" element={<Dashboard isAuthenticated={!!adminToken } className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/students" element={<Students isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/lecturers" element={<Lecturers isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/courses" element={<AdminCourse isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/news" element={<News isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/requests" element={<AdminRequest isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/grades" element={<Grade isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
-        <Route path="/admin/profile" element={<AdminProfile isAuthenticated={!!adminToken} className="SideMenuAndPageContent PageContent"/>} />
+        <Route
+          path="/admin"
+          element={
+            <AdminHome
+              isAuthenticated={!!adminToken}
+              adminToken={adminToken}
+              setAdminToken={setAdminToken}
+            />
+          }
+        />
+        <Route
+          path="/admin/login"
+          element={<AdminLogin setAdminToken={setAdminToken} />}
+        />
+        <Route
+          path="/admin/logout"
+          element={<AdminLogout setAdminToken={setAdminToken} />}
+        />
+        <Route
+          path="/admin/dashboard"
+          element={
+            <Dashboard
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/students"
+          element={
+            <Students
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/lecturers"
+          element={
+            <Lecturers
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/courses"
+          element={
+            <AdminCourse
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/news"
+          element={
+            <News
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/requests"
+          element={
+            <AdminRequest
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/grades"
+          element={
+            <Grade
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
+        <Route
+          path="/admin/profile"
+          element={
+            <AdminProfile
+              isAuthenticated={!!adminToken}
+              className="SideMenuAndPageContent PageContent"
+            />
+          }
+        />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
--- frontend/sgfrontend/src/components/Footer.jsx
+++ frontend/sgfrontend/src/components/Footer.jsx
@@ -1,7 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import { Typography } from "antd";
 function Footer() {
-  return <footer>Footer</footer>;
+  return (
+    <div className="AppFooter">
+      <Typography.Link href="tel:+123456789">+123456789</Typography.Link>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Privacy Policy
+      </Typography.Link>
+      <Typography.Link href="https://www.google.com" target={"_blank"}>
+        Terms of Use
+      </Typography.Link>
+    </div>
+  );
 }
 
 export default Footer;
