41865a0d20e33d93d30d207ec45152bbb8249114
Nguyen Le Linh Chi <chinguyencarat@gmail.com>
2024-01-04 10:38:36 +0700

    Change Login page

--- frontend/sgfrontend/src/components/Login.jsx
+++ frontend/sgfrontend/src/components/Login.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Form, Input, Button} from 'antd';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Input, Button } from "antd";
 import host from "../../config";
-
+import "../css/Login.css";
 function Login({ setToken, setModel, setId, setProfileHeader }) {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleSubmit = async () => {
@@ -13,9 +13,9 @@ function Login({ setToken, setModel, setId, setProfileHeader }) {
 
     // Make API request to login
     const response = await fetch(`${host}/api/login`, {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ username, password }),
     });
@@ -27,9 +27,9 @@ function Login({ setToken, setModel, setId, setProfileHeader }) {
       const id = data.id;
 
       // Save the token in localStorage
-      localStorage.setItem('token', token);
-      localStorage.setItem('model', model);
-      localStorage.setItem('id', id);
+      localStorage.setItem("token", token);
+      localStorage.setItem("model", model);
+      localStorage.setItem("id", id);
 
       // Update the token state
       setToken(token);
@@ -46,26 +46,26 @@ function Login({ setToken, setModel, setId, setProfileHeader }) {
               },
             }
           );
-  
+
           if (response.ok) {
             const data = await response.json();
             console.log(data);
-            if (model === 'student') {
+            if (model === "student") {
               const { name, student_id } = data;
               const newData = {
                 name,
-                student_id
+                student_id,
               };
               localStorage.setItem("profileHeader", JSON.stringify(newData));
-            } else if (model === 'lecturer') {
-                const { name, email } = data;
-                const newData = {
-                  name,
-                  email
-                };
-                localStorage.setItem("profileHeader", JSON.stringify(newData));
-              }
-              setProfileHeader(data);
+            } else if (model === "lecturer") {
+              const { name, email } = data;
+              const newData = {
+                name,
+                email,
+              };
+              localStorage.setItem("profileHeader", JSON.stringify(newData));
+            }
+            setProfileHeader(data);
           } else {
             navigate("/login");
           }
@@ -73,32 +73,66 @@ function Login({ setToken, setModel, setId, setProfileHeader }) {
           console.error(error);
         }
       };
-  
+
       fetchProfileData();
       // Redirect to the home page or any desired page
-      navigate('/');
+      navigate("/");
     } else {
       // Handle login error
-      console.error('Login failed');
+      console.error("Login failed");
     }
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <Form onFinish={handleSubmit}>
-        <Form.Item label="Username" name="username">
-          <Input placeholder='Username' id='username' value={username} onChange={(e) => setUsername(e.target.value)} required></Input>
-        </Form.Item>
-        <Form.Item label="Password" name="password">
-          <Input.Password placeholder='Password' id='password' value={password} onChange={(e) => setPassword(e.target.value)} required></Input.Password>
-        </Form.Item>
-        <Form.Item>
-          <Button block type='primary' htmlType='submit'>Login</Button>
-        </Form.Item>
-      </Form>
+    <div className="body">
+      <div className="container">
+        <div className="form-container sign-in-container">
+          <Form onFinish={handleSubmit}>
+            <h1>Sign in</h1>
+            <div className="align">
+              <Form.Item
+                name="username"
+                style={{ marginBottom: "6px", marginTop: "10px" }}
+              >
+                <Input
+                  className="input-field"
+                  placeholder="Username"
+                  id="username"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  required
+                ></Input>
+              </Form.Item>
+              <Form.Item name="password">
+                <Input.Password
+                  className="input-field"
+                  placeholder="Password"
+                  id="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                ></Input.Password>
+              </Form.Item>
+            </div>
+
+            <Form.Item>
+              <Button block type="primary" htmlType="submit">
+                Sign In
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+        <div className="overlay-container">
+          <div className="overlay">
+            <div className="overlay-panel overlay-right">
+              <h1>Hello, Friend!</h1>
+              <p>Welcome back!</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default Login
+export default Login;
